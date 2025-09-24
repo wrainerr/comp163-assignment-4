27efc00b27b4c846de493ac9314fd1de59df090f
Rayner Paulino-Payano <raynerpaulino2007@gmail.com>
2025-09-24 14:19:35 -0400

    Pass Test Case 1: Initialgame setup with variables

---
+++ rpaulinopayano_assignment_4.py
@@ -0,0 +1,13 @@
+student_name = "Rayner Paulino-Payano"
+current_gpa = 3.75
+study_hours = 20
+social_points = 50
+stress_levels = 30
+
+print("=== Welcome ===")
+print("Here are your starting stats:")
+print(f"Your name is: {student_name}")
+print(f"Your current gpa is: {current_gpa}")
+print(f"The number of hours you study per week is: {study_hours}")
+print(f"Your number of social points is (on a scale of 0 -100): {social_points}")
+print(f"Your current stress level is (on a scale of 0 - 100): {stress_levels}")
